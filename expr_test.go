6108302513e4031e4c763566737702aa345c13d1
wh8199 <819971938@qq.com>
2021-04-08 21:56:36 +0800

    Optimize parsing expression

--- expr_test.go
+++ expr_test.go
@@ -3,15 +3,7 @@ package expr
 import "testing"
 
 func TestParse(t *testing.T) {
-	tokens, err := tokenize([]rune("1 + (1 + 1)"))
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
-	t.Log(tokens)
-
-	expr, err := Parse("1 + (1 + a)", map[string]Var{
+	expr, err := Parse("1 + (1 * @.a)", map[string]Var{
 		"a": NewVar(Num(10.1)),
 	}, nil)
 	if err != nil {
